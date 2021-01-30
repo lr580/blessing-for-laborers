70337b84228fe7f02dd514d46fd54250e417b6c5
lr580 <2277379149@qq.com>
2021-01-30 12:13:21 +0800

    未剪枝版本

---
+++ main/miniprogram/components/searchBar/searchBar.js
@@ -0,0 +1,44 @@
+// components/chatroom/searchBar/searchBar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    showInput: function () {
+      this.setData({
+          inputShowed: true
+      });
+  },
+  hideInput: function () {
+      this.setData({
+          inputVal: "",
+          inputShowed: false
+      });
+  },
+  clearInput: function () {
+      this.setData({
+          inputVal: ""
+      });
+  },
+  inputTyping: function (e) {
+      this.setData({
+          inputVal: e.detail.value
+      });
+  }
+  }
+})
