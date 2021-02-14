9a9ada39b69eaf385099b5322a01823d718b0d2c
RyanMoriarty-lee <73153982+RyanMoriarty-lee@users.noreply.github.com>
2021-02-15 01:10:33 +0800

    Update collect.js

--- main/miniprogram/pages/collect/collect.js
+++ main/miniprogram/pages/collect/collect.js
@@ -1,5 +1,6 @@
 // pages/collect/collect.js
 const db=wx.cloud.database()
+const _=db.command
 var app = getApp();
 Page({
 
@@ -7,41 +8,85 @@ Page({
    * 页面的初始数据
    */
   data: {
+    datacollect:[],
     dataArr:[],
     name:null,
     tag:null,
     tutle:null
   },
 
-  getData(){
-    db.collection("user").doc(app.globalData.userID).get().then(res=>{
-
+  getData:async function(){
+    var res=await db.collection("user").doc(app.globalData.userID).get().then(res=>{
       this.setData({
         dataArr:res.data.collect
       })
+     
     })
   },
+  // collect:async function(){
+  //   var res=await this.getData()
+  //   for(var i=0,len=this.data.dataArr.length;i<len;i++){
+  //     var res= await db.collection("post").doc(String(this.data.dataArr[i])).get().then(res=>{
+  //       var type=null
+  //       var name
+  //       if(res.data.type==1){type="问答"}
+  //       else if(res.data.type==2){type="交流"}
+  //       else if(res.data.type==3){type="分享"}
+  //       else{type="日志"}
+  //       this.data.datacollect.push({title:res.data.title,tag:res.data.tag,actTime:res.data.activeTime,type:type})
+  //     })
+  //   }
+  // },
 
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad:function (options) {
-    this.getData();
+  onLoad:async function (options) {
+    // var res=await this.getData()
+    // for(var i=0,len=this.data.dataArr.length;i<len;i++){
+    //   var res= await db.collection("post").doc(String(this.data.dataArr[i])).get().then(res=>{
+    //     var type=null
+    //     var name
+    //     if(res.data.type==1){type="问答"}
+    //     else if(res.data.type==2){type="交流"}
+    //     else if(res.data.type==3){type="分享"}
+    //     else{type="日志"}
+    //     app.globalData.collect.push({title:res.data.title,tag:res.data.tag,actTime:res.data.activeTime,type:type})
+    //     console.log(app.globalData.collect)
+    //   })
+
+    // }
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: async function () {
+  onReady:  function () {
     
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow:  async function () {
-    
+  onShow:async function () {
+    var res=await this.getData()
+    for(var i=0,len=this.data.dataArr.length;i<len;i++){
+      var res= await db.collection("post").doc(String(this.data.dataArr[i])).get().then(res=>{
+        var type=null
+        var name
+        if(res.data.type==1){type="问答"}
+        else if(res.data.type==2){type="交流"}
+        else if(res.data.type==3){type="分享"}
+        else{type="日志"}
+        this.data.datacollect.push({title:res.data.title,tag:res.data.tag,pid:res.data._id,type:type})
+        this.setData({
+          datacollect:this.data.datacollect
+        })
+      })
+    }
+    console.log(this.data.datacollect)
   },
 
   /**
