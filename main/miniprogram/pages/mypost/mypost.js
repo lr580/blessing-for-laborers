684382488f9bbe6684a4fa316c09a8def043ef57
RyanMoriarty-lee <73153982+RyanMoriarty-lee@users.noreply.github.com>
2021-02-15 21:04:49 +0800

    完善我的帖子js

--- main/miniprogram/pages/mypost/mypost.js
+++ main/miniprogram/pages/mypost/mypost.js
@@ -1,32 +1,62 @@
-// pages/mypost/mypost.js
+// pages/collect/collect.js
+const db=wx.cloud.database()
+const _=db.command
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    mypost:[],
+    dataArr:[],
+    name:null,
+    tag:null,
+    tutle:null
   },
 
+  getData:async function(){
+    var res=await db.collection("user").doc(app.globalData.userID).get().then(res=>{
+      this.setData({
+        dataArr:res.data.publish
+      })
+     
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad:async function (options) {
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady:  function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow:async function () {
+    var res=await this.getData()
+    for(var i=0,len=this.data.dataArr.length;i<len;i++){
+      var res= await db.collection("post").doc(String(this.data.dataArr[i])).get().then(res=>{
+        var type=null
+        var name
+        if(res.data.type==1){type="问答"}
+        else if(res.data.type==2){type="交流"}
+        else if(res.data.type==3){type="分享"}
+        else{type="日志"}
+        this.data.mypost.push({title:res.data.title,tag:res.data.tag,pid:res.data._id,type:type})
+        this.setData({
+          mypost:this.data.mypost
+        })
+      })
+    }
   },
 
   /**
